7b76d7eeb0d6855dfd5fded8d0c8c74c473588c6
Corey Elowsky <elowsky@CSHL.edu>
2022-02-22 16:36:40 -0500

    generate xml script

--- generate_xml.py
+++ generate_xml.py
@@ -1,8 +1,7 @@
 from stitching.StitchingXML import StitchingXML
 from os.path import join
 
-
-########## Parameters ############
+########## Parameters ###########
 xml_path = '/mnt/nfs/grids/hpc_norepl/qi/data/AVP/AVP-IHC-A3/downsample2/'
 xml_name = 'estimate_overlaps.xml'
 overlaps = {'x':6.1, 'y':22.5, 'z':99}
