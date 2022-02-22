f70aea465e525ee2895e14d8b804466660117ec5
Corey Elowsky <elowsky@CSHL.edu>
2022-02-22 16:33:25 -0500

    generate xml script

---
+++ generate_xml.py
@@ -0,0 +1,24 @@
+from stitching.StitchingXML import StitchingXML
+from os.path import join
+
+
+########## Parameters ############
+xml_path = '/mnt/nfs/grids/hpc_norepl/qi/data/AVP/AVP-IHC-A3/downsample2/'
+xml_name = 'estimate_overlaps.xml'
+overlaps = {'x':6.1, 'y':22.5, 'z':99}
+#################################
+
+# create stitchign xml object
+xml = StitchingXML(join(xml_path, xml_name))	
+
+# sets overlaps in xml
+xml.set_translation_to_grid_overlaps(overlaps)
+
+# saves xml
+xml_out_name = f'estimate_overlaps_{overlaps["x"]}_{overlaps["y"]}_{overlaps["z"]}'
+xml.save_xml(xml_out_name)
+
+# generate report (.txt) for new xml
+xml = StitchingXML(join(xml_path, xml_out_name + '.xml'))	
+xml.generate_report()
+
